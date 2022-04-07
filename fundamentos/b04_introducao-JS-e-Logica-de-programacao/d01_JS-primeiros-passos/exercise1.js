464b01430d697f7be6241d2d3adab6106a3d7111
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-07 09:46:06 -0300

    Exercise 1 of Bloco 4 - Dia 01

---
+++ fundamentos/b04_introducao-JS-e-Logica-de-programacao/d01_JS-primeiros-passos/exercise1.js
@@ -0,0 +1,21 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+const a = 41;
+const b = 38;
+
+let sum = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+let mod = a % b;
+
+console.log("The sum is =", sum);
+console.log("The subtraction is =", sub);
+console.log("The multiplication is =", mult);
+console.log("The division is =", div);
+console.log("The module is =", mod);
