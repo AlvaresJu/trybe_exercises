f74b16645881ee401ae77cf654443fd57301ecff
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-07 18:48:45 -0300

    Complement file comments

--- fundamentos/b04_introducao-JS-e-Logica-de-programacao/d01_JS-primeiros-passos/exercise6.js
+++ fundamentos/b04_introducao-JS-e-Logica-de-programacao/d01_JS-primeiros-passos/exercise6.js
@@ -1,4 +1,6 @@
-/* Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz. */
+/* Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz. 
+Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.*/
 
 const chessPice = "Queen";
 const lowerCasePice = chessPice.toLowerCase();
