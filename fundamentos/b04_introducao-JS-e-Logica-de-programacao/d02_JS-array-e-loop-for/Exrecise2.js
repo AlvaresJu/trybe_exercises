0e3a3b07125dd3c95e3ae6bfa17fa6000019700e
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-08 11:09:02 -0300

    Adjust code formatting - Ex. 1 and 2

--- fundamentos/b04_introducao-JS-e-Logica-de-programacao/d02_JS-array-e-loop-for/Exrecise2.js
+++ fundamentos/b04_introducao-JS-e-Logica-de-programacao/d02_JS-array-e-loop-for/Exrecise2.js
@@ -1,4 +1,4 @@
-/* Para o segundo exercício, some todos os valores contidos no array e imprima o resultado */
+/* Para o segundo exercício, some todos os valores contidos no array e imprima o resultado. */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
