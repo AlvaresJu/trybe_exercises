37c3b308ac36912079022b9ae9ec8821171740e9
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-10 19:41:18 -0300

    Exercise 2 of Bloco 04 - Dia 03

---
+++ fundamentos/b04_introducao-JS-e-Logica-de-programacao/d03_JS-logica-de-programacao-e-algoritmos/exercise2.js
@@ -0,0 +1,11 @@
+/* Desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente. */
+
+let word = 'trybe';
+
+let reversedWord = "";
+
+for (i = word.length - 1; i >= 0; i -= 1) {
+    reversedWord += word[i];
+}
+
+console.log(reversedWord);
