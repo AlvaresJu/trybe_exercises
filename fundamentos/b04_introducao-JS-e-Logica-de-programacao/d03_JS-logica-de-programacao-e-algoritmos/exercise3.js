3e4e5d2669dcaedd9a4e6bedbca7716e805046fe
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-10 19:55:24 -0300

    exercise 3 of Bloco 04 - Dia 03

---
+++ fundamentos/b04_introducao-JS-e-Logica-de-programacao/d03_JS-logica-de-programacao-e-algoritmos/exercise3.js
@@ -0,0 +1,19 @@
+/* Considere um array de strings. Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra. */
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (i = 1; i < array.length; i += 1) {
+    if (array[i].length > biggestWord.length) {
+        biggestWord = array[i];
+    }
+
+    if (array[i].length < smallestWord.length) {
+        smallestWord = array[i];
+    }
+}
+
+console.log("The biggest word is: " + biggestWord);
+console.log("The smallest word is: " + smallestWord);
