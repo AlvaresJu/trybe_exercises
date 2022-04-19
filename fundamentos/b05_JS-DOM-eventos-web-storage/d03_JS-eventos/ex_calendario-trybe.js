00d01e559130ab49bce907ff3c040245c027b544
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-19 10:52:26 -0300

    Requirement 1 of exercise 'calendário Trybe' - Bloco 05 - Day 03

--- fundamentos/b05_JS-DOM-eventos-web-storage/d03_JS-eventos/ex_calendario-trybe.js
+++ fundamentos/b05_JS-DOM-eventos-web-storage/d03_JS-eventos/ex_calendario-trybe.js
@@ -9,8 +9,43 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+/* Exercício 1 - O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days". Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+
+Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+
+Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+
+Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li> */
+
+function dezCalendarDays () {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const dezList = document.getElementById('days');
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        let dezDay = document.createElement('li');
+        dezDay.innerHTML = dezDaysList[index];
+        dezList.appendChild(dezDay);
+
+        switch (dezDaysList[index]) {
+            case 24:
+            case 31:
+                dezDay.classList.add('day', 'holiday');
+                break;
+            case 4:
+            case 11:
+            case 18:
+                dezDay.classList.add('day', 'friday');
+                break;
+            case 25:
+                dezDay.classList.add('day', 'holiday', 'friday');
+                break;
+            default:
+                dezDay.className = 'day';
+        }
+    }
+}
+
+dezCalendarDays ();
