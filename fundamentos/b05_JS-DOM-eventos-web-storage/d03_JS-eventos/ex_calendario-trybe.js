b7bd8128f69cf6769fd5f221291f96ab8a6a5e93
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-20 09:32:40 -0300

    Requirement 8 of exercise 'calendário Trybe' - Bloco 5 - Day 03

--- fundamentos/b05_JS-DOM-eventos-web-storage/d03_JS-eventos/ex_calendario-trybe.js
+++ fundamentos/b05_JS-DOM-eventos-web-storage/d03_JS-eventos/ex_calendario-trybe.js
@@ -155,4 +155,20 @@ function addTask (task) {
     myTasks.appendChild(newTask);
 }
 
-addTask ('Cozinhar');
+addTask ('Projeto Trybe');
+
+/* Exercício 8: Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task.
+
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks". */
+
+function taskLabel (colorName) {
+    const label = document.createElement('div');
+    const myTasks = document.querySelector('.my-tasks');
+    myTasks.appendChild(label);
+    label.className = 'task';
+    label.style.backgroundColor = colorName;
+}
+
+taskLabel ('salmon');
