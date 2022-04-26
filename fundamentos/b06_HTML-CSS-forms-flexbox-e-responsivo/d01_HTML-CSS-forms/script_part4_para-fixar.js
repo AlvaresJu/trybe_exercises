1dc863f571ac135d94fd107f72c997fabdc98a82
Juliana Alvares <alvares.juliana@hotmail.com>
2022-04-26 16:00:55 -0300

    Requirement 1 of exercise Part IV - Bloco 06 - Day 01

--- fundamentos/b06_HTML-CSS-forms-flexbox-e-responsivo/d01_HTML-CSS-forms/script_part4_para-fixar.js
+++ fundamentos/b06_HTML-CSS-forms-flexbox-e-responsivo/d01_HTML-CSS-forms/script_part4_para-fixar.js
@@ -1,3 +1,7 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click', function(event) {
+    event.preventDefault();
+});
