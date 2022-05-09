50b7bee281552db9146bf9d12965cffd7c7845cc
Juliana Alvares <alvares.juliana@hotmail.com>
2022-05-08 21:10:04 -0300

    Exercise 1 of Part II - Bloco 07 - Day 01

---
+++ fundamentos/b07_introducao-javaScript-ES6-e-testes-unitarios/d01_JavaScript-ES6-let-const-arrow-functions-template-literals/ex1_7.1_parte2.js
@@ -0,0 +1,16 @@
+/* Crie uma função que receba um número e retorne seu fatorial. (Obs: É fundamental que você utilize o que aprendeu sobre let, const, arrow functions, template literals e ternary operator.) */
+
+const fatorial = (number) => {
+  let result = 1;
+  if (number < 0) {
+    console.log(`O número ${number} não possui fatorial`);
+    return;
+  } else if (number > 0) {
+    for (let n = 1; n <= number; n += 1) {
+      result *= n;
+    }
+  }
+  return console.log(`${number}! = ${result}`);
+}
+
+fatorial(-9);
