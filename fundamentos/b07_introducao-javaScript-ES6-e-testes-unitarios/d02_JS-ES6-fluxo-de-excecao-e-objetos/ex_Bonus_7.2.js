a97617b800c041c434666a213d2e1cc3a484a786
Juliana Alvares <alvares.juliana@hotmail.com>
2022-05-11 11:33:32 -0300

    Exercise bonus 2 of Bloco 07 - Day 02

--- fundamentos/b07_introducao-javaScript-ES6-e-testes-unitarios/d02_JS-ES6-fluxo-de-excecao-e-objetos/ex_Bonus_7.2.js
+++ fundamentos/b07_introducao-javaScript-ES6-e-testes-unitarios/d02_JS-ES6-fluxo-de-excecao-e-objetos/ex_Bonus_7.2.js
@@ -1,4 +1,4 @@
-// 🚀 Crie uma função para contar quantos estudantes assistiram às aulas de Matemática, utilizando o objeto a seguir:
+// 1. 🚀 Crie uma função para contar quantos estudantes assistiram às aulas de Matemática, utilizando o objeto a seguir:
 
 const allLessons = {
   lesson1:
@@ -30,4 +30,22 @@ const allLessons = {
  }
 
  console.log(countMathStudents(allLessons));
- 
+
+//  2. 🚀 Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o mesmo objeto de entrada do exercício anterior:
+
+function createReport (allLessons, professor) {
+  const arrayOfLessons = Object.values(allLessons);
+  let students = 0;
+  const lessons = [];
+  for (let index = 0; index < arrayOfLessons.length; index += 1) {
+    if (arrayOfLessons[index].professor === professor) {
+      students += arrayOfLessons[index].numeroEstudantes;
+      lessons.push(arrayOfLessons[index].materia);
+    }
+  }
+  return {professor: `${professor}`,
+          aulas: lessons,
+          estudantes: students};
+}
+
+console.log(createReport(allLessons, 'Maria Clara'));
