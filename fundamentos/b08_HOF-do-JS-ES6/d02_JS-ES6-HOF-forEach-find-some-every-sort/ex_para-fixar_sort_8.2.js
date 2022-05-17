ac235a8801de62c608905e8884925ffce849c410
Juliana Alvares <alvares.juliana@hotmail.com>
2022-05-16 22:46:11 -0300

    Exercise 2 of 'Para fixar - Array.sort' of Bloco 08 - Day 02

--- fundamentos/b08_HOF-do-JS-ES6/d02_JS-ES6-HOF-forEach-find-some-every-sort/ex_para-fixar_sort_8.2.js
+++ fundamentos/b08_HOF-do-JS-ES6/d02_JS-ES6-HOF-forEach-find-some-every-sort/ex_para-fixar_sort_8.2.js
@@ -12,3 +12,16 @@ people.sort((personA, personB) => personA.age - personB.age);
 
 console.log(people);
 
+// 2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+
+people.sort((personA, personB) => personB.age - personA.age);
+
+console.log(people);
+
+// Minha adição ao exercício: ordenar o array por ordem alfabética do nome das pessoas.
+
+console.log(people.sort((personA, personB) => {
+  if (personA.name < personB.name) return -1;
+  if (personA.name > personB.name) return 1;
+  return 0;
+}));
