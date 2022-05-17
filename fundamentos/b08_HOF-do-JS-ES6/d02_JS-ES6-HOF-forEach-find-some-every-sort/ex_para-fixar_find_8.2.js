ea0557906ad45df5493419ac612c798f11939f99
Juliana Alvares <alvares.juliana@hotmail.com>
2022-05-16 22:11:54 -0300

    Exercise 3 of 'Para fixar - Array.find' of Bloco 08 - Day 02

--- fundamentos/b08_HOF-do-JS-ES6/d02_JS-ES6-HOF-forEach-find-some-every-sort/ex_para-fixar_find_8.2.js
+++ fundamentos/b08_HOF-do-JS-ES6/d02_JS-ES6-HOF-forEach-find-some-every-sort/ex_para-fixar_find_8.2.js
@@ -17,3 +17,17 @@ const findNameWithFiveLetters = (names) => {
 }
 
 console.log(findNameWithFiveLetters(names));
+
+// 3 - Utilize o find para encontrar a música com id igual a 31031685, caso ela exista:
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id, musicList) {
+  return musicList.find((music) => music.id === id);
+}
+
+console.log(findMusic('31031685', musicas));
