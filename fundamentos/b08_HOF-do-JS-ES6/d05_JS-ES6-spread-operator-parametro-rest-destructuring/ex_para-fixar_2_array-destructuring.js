eab94384554d1b9746b5b06ca449ecee68e3576e
Juliana Alvares <alvares.juliana@hotmail.com>
2022-05-19 15:47:36 -0300

    Exercise 2 of 'Para fixar - array destructuring' of Bloco 08 - Day 05

---
+++ fundamentos/b08_HOF-do-JS-ES6/d05_JS-ES6-spread-operator-parametro-rest-destructuring/ex_para-fixar_2_array-destructuring.js
@@ -0,0 +1,12 @@
+// 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+// [comida = 'arroz', animal = 'gato', bebida = 'água'];
+
+// Ou sriando o array e o desestruturando:
+// [comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida);
