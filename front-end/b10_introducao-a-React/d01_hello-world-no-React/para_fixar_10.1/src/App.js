8d81ddc86078a36ca378288c41c3038e91b7bdcf
Juliana Alvares <alvares.juliana@hotmail.com>
2022-06-10 14:35:57 -0300

    Exercise 'Para fixar - JSX' of Bloco 10 - Day 01

---
+++ front-end/b10_introducao-a-React/d01_hello-world-no-React/para_fixar_10.1/src/App.js
@@ -0,0 +1,9 @@
+import ParaFixarJSX from "./ParaFixarJSX";
+
+function App() {
+  return (
+    <ParaFixarJSX />
+  );
+}
+
+export default App;
---
+++ front-end/b10_introducao-a-React/d01_hello-world-no-React/para_fixar_10.1/src/ParaFixarJSX.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ParaFixarJSX extends Component {
+  render() {
+    const textJSX = 'Hello, JSX!';
+    return (
+      <div>
+        <h1>Para Fixar - JSX</h1>
+        <h2>{textJSX}</h2>
+      </div>
+    );
+  }
+}
+
+export default ParaFixarJSX;
