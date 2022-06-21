d65500c3fc8e49512257fb8211d31b545208c8ac
Juliana Alvares <alvares.juliana@hotmail.com>
2022-06-20 21:26:42 -0300

    Fixation exercise about 'Component composition' of Part 10 - Day 02

--- front-end/b10_introducao-a-React/d02_componentes-React/fixation-exercises-10-2/src/App.js
+++ front-end/b10_introducao-a-React/d02_componentes-React/fixation-exercises-10-2/src/App.js
@@ -1,8 +1,13 @@
 import Image from "./Image";
+import OrderList from "./OrderList";
 
 function App() {
   return (
-    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+    <>
+      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+      <br></br>
+      <OrderList />
+    </>
   );
 }
 
