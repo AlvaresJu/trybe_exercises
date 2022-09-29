7c562b9f9cdf2a16e3e4e00c06a32459edf9d9b3
Juliana Alvares <alvares.juliana@hotmail.com>
2022-09-29 11:03:28 -0300

    Exercise 05 (add BMI status) of part 22 - day 01

--- back-end/b22_Intro-web-dev-com-Node/d01_Node-Runtime-assincrono/my-scripts/src/bmi.js
+++ back-end/b22_Intro-web-dev-com-Node/d01_Node-Runtime-assincrono/my-scripts/src/bmi.js
@@ -2,7 +2,16 @@ const readline = require('readline-sync');
 
 function calculatesBMI(weight, height) {
   const bmi = weight / (height ** 2);
-  return bmi.toFixed(2);
+  return bmi;
+}
+
+function setBMICategory(bmi) {
+  if (bmi < 18.5) return 'Underweight (thin)';
+  if (bmi >= 18.5 && bmi < 25) return 'Normal weight';
+  if (bmi >= 25 && bmi < 30) return 'Overweight';
+  if (bmi >= 30 && bmi < 35) return 'Grade I obesity';
+  if (bmi >= 35 && bmi < 40) return 'Grade II obesity';
+  return 'Grade III and IV obesity';
 }
 
 function main() {
@@ -11,8 +20,11 @@ function main() {
 
   console.log(`Weight: ${weight} kg, Height: ${height}0 m`);
 
-  const bmi = calculatesBMI(weight, height);
-  console.log(`BMI = ${bmi}`);
+  const calculatedBMI = calculatesBMI(weight, height);
+  console.log(`BMI = ${calculatedBMI.toFixed(2)}`);
+
+  const bmiCategory = setBMICategory(calculatedBMI);
+  console.log(`BMI status = ${bmiCategory}`);
 }
 
 main();
