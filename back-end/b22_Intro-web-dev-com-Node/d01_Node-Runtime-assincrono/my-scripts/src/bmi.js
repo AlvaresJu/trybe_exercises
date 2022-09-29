09c4c2bf5e1116b1cf4a9e67c7025e33d2d794e7
Juliana Alvares <alvares.juliana@hotmail.com>
2022-09-29 10:33:56 -0300

    Exercises 03 and 04 (edit bmi function to receive dinamic params) of part 22 - day 01

--- back-end/b22_Intro-web-dev-com-Node/d01_Node-Runtime-assincrono/my-scripts/src/bmi.js
+++ back-end/b22_Intro-web-dev-com-Node/d01_Node-Runtime-assincrono/my-scripts/src/bmi.js
@@ -1,11 +1,17 @@
+const readline = require('readline-sync');
+
 function calculatesBMI(weight, height) {
   const bmi = weight / (height ** 2);
   return bmi.toFixed(2);
 }
 
 function main() {
-  console.log('Weight: 63 kg, Height: 1.70 m');
-  const bmi = calculatesBMI(63, 1.70);
+  const weight = readline.questionFloat('What’ your weight in Kg? ');
+  const height = readline.questionFloat('What’ your height in m? ');
+
+  console.log(`Weight: ${weight} kg, Height: ${height}0 m`);
+
+  const bmi = calculatesBMI(weight, height);
   console.log(`BMI = ${bmi}`);
 }
 
