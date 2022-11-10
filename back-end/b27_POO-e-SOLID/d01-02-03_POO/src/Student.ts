29f3dc4ddd4387cc5ef75dcf80c7fb55d5ab62fc
Juliana Alvares <alvares.juliana@hotmail.com>
2022-11-10 19:10:02 -0300

    Exercise 1 - part 27 - day 01

---
+++ back-end/b27_POO-e-SOLID/d01-02-03_POO/src/Student.ts
@@ -0,0 +1,8 @@
+export default class Student {
+  constructor(
+    private _registration: string,
+    private _name: string,
+    private _testScores: number[],
+    private _workScores: number[],
+  ) { }
+}
