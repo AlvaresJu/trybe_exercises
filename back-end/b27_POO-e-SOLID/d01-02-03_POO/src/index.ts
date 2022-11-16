e04d15382487799f983d3390e1c9832ec12d6309
Juliana Alvares <alvares.juliana@hotmail.com>
2022-11-16 19:37:00 -0300

    Exercises 3 and 4 - part 27 - day 01

--- back-end/b27_POO-e-SOLID/d01-02-03_POO/src/index.ts
+++ back-end/b27_POO-e-SOLID/d01-02-03_POO/src/index.ts
@@ -1,5 +1,13 @@
 import Student from './Student';
 
-const student1 = new Student('teste', 'sdfs');
+const student1 = new Student('1516651312341545', 'Maria de Amaral');
+student1.testScores = [24, 1, 45, 47];
+student1.workScores = [42, 44];
 
 console.log(student1);
+console.log(student1.calculateAverageScore());
+console.log(student1.calculateTotalScore());
+
+const student2 = new Student('4654324132135645', 'Joaquina Pereira');
+
+console.log(student2);
