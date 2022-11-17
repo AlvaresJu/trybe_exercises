ce707bc37ac60a91c7c45d517cfb8eb87f243f6a
Juliana Alvares <alvares.juliana@hotmail.com>
2022-11-17 19:03:15 -0300

    Exercise 1 - part 27 - day 02

--- back-end/b27_POO-e-SOLID/d01-02-03_POO/src/index.ts
+++ back-end/b27_POO-e-SOLID/d01-02-03_POO/src/index.ts
@@ -1,4 +1,5 @@
 import Student from './Student';
+import Person from './Person';
 
 const student1 = new Student('1516651312341545', 'Maria de Amaral');
 student1.testScores = [24, 1, 45, 47];
@@ -11,3 +12,8 @@ console.log(student1.calculateTotalScore());
 const student2 = new Student('4654324132135645', 'Joaquina Pereira');
 
 console.log(student2);
+
+const birthDate = new Date('1994/01/25');
+const person1 = new Person('juliana', birthDate);
+
+console.log(person1);
